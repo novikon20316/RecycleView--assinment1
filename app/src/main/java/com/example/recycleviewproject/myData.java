bb675f9521c7b46983ee466e3dbc119d4cb47439
novikon20316 <dor.novik15@gmail.com>
2024-12-28 11:36:13 +0200

    final project

---
+++ app/src/main/java/com/example/recycleviewproject/myData.java
@@ -0,0 +1,33 @@
+package com.example.recycleviewproject;
+
+public class myData {
+    static String[] nameArray = {"Aladin", "Ang", "Meny", "Mickey", "Nala", "Pinokio", "Scar","Shilgia", "Sonic", "Tiles"};
+    static String[] versionArray = {
+            "I’m a prince that finds a lamp.",
+            "I am the Avatar. Peacekeeper’s duty is my responsible",
+            "I’m big strong mammoth and I have tusks",
+            "He is a mouse that loves Minnie.",
+            "She’s a lioness, and loves Simba",
+            "I’m a puppet that every time i lie my nose grows",
+            "He's a Lion, and Full of Evil",
+            "I lived with Seven dwarfs and I ate the poisonous apple",
+            "I'm blue and I run fast!",
+            "Im sonic's best friend and I can fly"
+    };
+
+    static Integer[] drawableArray = {
+            R.drawable.aladin,
+            R.drawable.ang,
+            R.drawable.meny,
+            R.drawable.mickey,
+            R.drawable.nala,
+            R.drawable.pinokio,
+            R.drawable.scar,
+            R.drawable.shilgia,
+            R.drawable.sonic,
+            R.drawable.tiles
+    };
+
+    static Integer[] _id = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+}
